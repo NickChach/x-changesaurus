e5d6f2067cf2984a7f128c503565932a026b873a
Nick Chachlioutis <debian5user@gmail.com>
2022-06-28 16:45:56 +0300

    service worker caches API data

--- service-worker.js
+++ service-worker.js
@@ -18,9 +18,25 @@ self.addEventListener("fetch", event => {
     if (url.origin == location.origin) {
         event.respondWith(cacheFirst(request));
     }
+    else {
+        event.respondWith(networkFirst(request));
+    }
 });
 
 async function cacheFirst(request) {
     const response = await caches.match(request);
     return response || fetch(request);
+}
+
+async function networkFirst(request) {
+    const cache = await caches.open("exchangerate-api");
+    try {
+        const response = await fetch(request);
+        cache.put(request, response.clone());
+        return response;
+    }
+    catch (error) {
+        const response = await cache.match(request);
+        return response;
+    }
 }
